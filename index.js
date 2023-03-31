85085bfefc9521881e38821c27d32d0867e4bed2
luckjjh <luckjjh1@naver.com>
2023-03-31 21:20:12 +0900

    서버 연동 코드 추가

--- index.js
+++ index.js
@@ -11,7 +11,140 @@ const {
   ButtonBuilder,
   ButtonStyle,
 } = require("discord.js");
+const dbClient = require("@notionhq/client");
 const { token } = require("./config-dev.json"); //commit 시 수정
+const dbID = "adc986585ab64b5693b9399334c7d935";
+const { auth, notionVersion } = require("./db_key");
+const notion = new dbClient.Client({
+  auth: auth,
+  notionVersion: notionVersion,
+});
+
+let top3List = [];
+let team1Temp = [];
+let team2Temp = [];
+let checkDelay = false;
+
+async function getAllUserData() {
+  const res = await notion.databases.query({
+    database_id: dbID,
+  });
+  return res.results;
+}
+
+async function getTop3(dir) {
+  top3List = [];
+  const res = await notion.databases.query({
+    database_id: dbID,
+    sorts: [
+      {
+        property: "power",
+        direction: dir ? "descending" : "ascending",
+      },
+    ],
+  });
+  let cnt = 0;
+  res.results.forEach((item) => {
+    if (cnt === 3) {
+      return;
+    }
+    top3List.push(item.properties);
+    cnt += 1;
+  });
+  return top3List;
+}
+
+async function addToDatabase(databaseId, username) {
+  let check = false;
+  const res = await notion.search({
+    query: username,
+  });
+  if (res.results.length === 0) {
+    check = true;
+  }
+  if (check) {
+    try {
+      await notion.pages.create({
+        parent: {
+          database_id: databaseId,
+        },
+        properties: {
+          name: {
+            type: "title",
+            title: [
+              {
+                type: "text",
+                text: {
+                  content: username,
+                },
+              },
+            ],
+          },
+          win: {
+            type: "number",
+            number: 0,
+          },
+          lose: {
+            type: "number",
+            number: 0,
+          },
+          power: {
+            type: "number",
+            number: 0,
+          },
+        },
+      });
+    } catch (error) {
+      console.error(error.body);
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
+
+async function searchUser(userName) {
+  const res = await notion.search({
+    query: userName,
+  });
+  return res;
+}
+
+async function updateValue(originalData, state) {
+  switch (state) {
+    case "win":
+      await notion.pages.update({
+        page_id: originalData.results[0].id,
+        properties: {
+          win: {
+            type: "number",
+            number: (originalData.results[0].properties.win.number += 1),
+          },
+          power: {
+            type: "number",
+            number: (originalData.results[0].properties.power.number += 1),
+          },
+        },
+      });
+      break;
+    case "lose":
+      await notion.pages.update({
+        page_id: originalData.results[0].id,
+        properties: {
+          lose: {
+            type: "number",
+            number: (originalData.results[0].properties.lose.number += 1),
+          },
+          power: {
+            type: "number",
+            number: (originalData.results[0].properties.power.number -= 1),
+          },
+        },
+      });
+      break;
+  }
+}
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -47,6 +180,21 @@ client.on("error", (err) => {
   console.log(err.message);
 });
 
+async function registerUser(message) {
+  let onlineUserArr = [];
+  message.guild.members.fetch().then((fetchedMembers) => {
+    fetchedMembers.forEach((k, v) => {
+      let curMem = message.guild.members.cache.get(k.id);
+      if (curMem.voice.channel) {
+        onlineUserArr.push(k.user.username);
+      }
+    });
+    onlineUserArr.forEach(async (item) => {
+      await addToDatabase(dbID, item);
+    });
+  });
+}
+
 function makeTeam(message) {
   let onlineUserArr = [];
   message.guild.members.fetch().then((fetchedMembers) => {
@@ -75,8 +223,8 @@ function makeTeam(message) {
       const team1 = sortedUserWithKey.splice(0, teamCount);
       const team2 = sortedUserWithKey.splice(-teamCount);
 
-      let team1Temp = [];
-      let team2Temp = [];
+      team1Temp = [];
+      team2Temp = [];
       team1.forEach((usr) => {
         team1Temp.push(usr.name);
       });
@@ -96,7 +244,7 @@ function makeTeam(message) {
   });
 }
 
-client.on("messageCreate", (message) => {
+client.on("messageCreate", async (message) => {
   if (message.author.username === "kwonSM") {
     message.react("💩");
   }
@@ -129,27 +277,103 @@ client.on("messageCreate", (message) => {
   }
   if (message.content == "!team") {
     makeTeam(message);
+    setTimeout(() => {
+      checkDelay = true;
+    }, 60000);
+  }
+  if (message.content == "!top3") {
+    const top3Data = await getTop3(true);
+    const exampleEmbed = new EmbedBuilder()
+      .setColor(0x0099ff)
+      .setTitle("Top 3👑")
+      .addFields(
+        {
+          name: "1️⃣등",
+          value: `${top3Data[0].name.title[0].text.content} ${top3Data[0].power.number} 롤투력`,
+        },
+        {
+          name: "2️⃣등",
+          value: `${top3Data[1].name.title[0].text.content} ${top3Data[1].power.number} 롤투력`,
+        },
+        {
+          name: "3️⃣등",
+          value: `${top3Data[2].name.title[0].text.content} ${top3Data[2].power.number} 롤투력`,
+        }
+      );
+    message.reply({ embeds: [exampleEmbed] });
+  }
+  if (message.content == "!register") {
+    await registerUser(message);
+    message.reply("등록까지 1~2분 소요됩니다.");
+  }
+  if (message.content == "!showAll") {
+    const userData = await getAllUserData();
+    let allData = [];
+    userData.forEach((item) => {
+      const percent =
+        (item.properties.win.number /
+          (item.properties.lose.number + item.properties.win.number)) *
+        100;
+      allData.push({
+        name: `이름: ${item.properties.name.title[0].text.content}`,
+        value: `승: ${item.properties.win.number} 
+          패: ${item.properties.lose.number} 
+          파워: ${item.properties.power.number} 
+          승률: ${percent}`,
+      });
+    });
+    const exampleEmbed = new EmbedBuilder()
+      .setColor(0x0099ff)
+      .setTitle("All user")
+      .addFields(allData);
+    message.reply({ embeds: [exampleEmbed] });
   }
 });
 
 client.on("interactionCreate", async (interaction) => {
   if (interaction.isButton()) {
-    console.log("button Clicked");
     if (interaction.component.data.custom_id === "rerollBtn") {
       interaction.reply(
         `**${interaction.user.username}**님이 '리롤 버튼'을 클릭했습니다.`
       );
       makeTeam(interaction);
-    } else if (interaction.component.data.custom_id === "team1winBtn") {
+    } else if (
+      interaction.component.data.custom_id === "team1winBtn" &&
+      checkDelay
+    ) {
       interaction.reply(
         `**${interaction.user.username}**님이 '1팀 승리 버튼'을 클릭했습니다.`
       );
-    } else if (interaction.component.data.custom_id === "team2winBtn") {
+      team1Temp.forEach(async (user) => {
+        const userData1 = await searchUser(user);
+        await updateValue(userData1, "win");
+      });
+      team2Temp.forEach(async (user) => {
+        const userData2 = await searchUser(user);
+        await updateValue(userData2, "lose");
+      });
+    } else if (
+      interaction.component.data.custom_id === "team2winBtn" &&
+      checkDelay
+    ) {
       interaction.reply(
         `**${interaction.user.username}**님이 '2팀 승리 버튼'을 클릭했습니다.`
       );
+      team1Temp.forEach(async (user) => {
+        const userData1 = await searchUser(user);
+        await updateValue(userData1, "lose");
+      });
+      team2Temp.forEach(async (user) => {
+        const userData2 = await searchUser(user);
+        await updateValue(userData2, "win");
+      });
     }
-    await interaction.message.delete();
+    if (checkDelay) {
+      await interaction.message.delete();
+      checkDelay = false;
+    } else {
+      interaction.reply("1분 뒤에 동작 가능합니다.");
+    }
   }
 });
 
