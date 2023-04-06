ffddcfc4c4692854b4251f3337e4a640e3171dbb
luckjjh <luckjjh1@naver.com>
2023-04-06 22:54:30 +0900

    [fix] DB import

--- command.js
+++ command.js
@@ -1,64 +1,65 @@
 const {
-    Client,
-    Events,
-    GatewayIntentBits,
-    GuildMemberManager,
-    GuildMember,
-    Guild,
-    Partials,
-    EmbedBuilder,
-    ActionRowBuilder,
-    ButtonBuilder,
-    ButtonStyle,
+  Client,
+  Events,
+  GatewayIntentBits,
+  GuildMemberManager,
+  GuildMember,
+  Guild,
+  Partials,
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
 } = require("discord.js");
 
+const DB = require("./db_api.js");
+var exports = (module.exports = {});
 
-var exports = module.exports = {};
+exports.rollDice = function () {
+  return Math.floor(Math.random() * 100);
+};
 
-exports.rollDice= function(){
-    return Math.floor(Math.random() * 100);
-}
-
-exports.registerUser = async function(message) {
-    let onlineUserArr = [];
-    message.guild.members.fetch().then((fetchedMembers) => {
-      fetchedMembers.forEach((k, v) => {
-        let curMem = message.guild.members.cache.get(k.id);
-        if (curMem.voice.channel) {
-          onlineUserArr.push(k.user.username);
-        }
-      });
-      onlineUserArr.forEach(async (item) => {
-        await DB.addToDatabase(item);
-      });
+exports.registerUser = async function (message) {
+  let onlineUserArr = [];
+  message.guild.members.fetch().then((fetchedMembers) => {
+    fetchedMembers.forEach((k, v) => {
+      let curMem = message.guild.members.cache.get(k.id);
+      if (curMem.voice.channel) {
+        onlineUserArr.push(k.user.username);
+      }
     });
-}
+    onlineUserArr.forEach(async (item) => {
+      await DB.addToDatabase(item);
+    });
+  });
+};
 
-exports.makeTeam =async function(message) {
-  var channel = await  message.guild.channels.fetch(message.member.voice.channel.id);
-  if (channel.members.size % 2 === 1){
+exports.makeTeam = async function (message) {
+  var channel = await message.guild.channels.fetch(
+    message.member.voice.channel.id
+  );
+  if (channel.members.size % 2 === 1) {
     return null;
-  }
-  else{
-    let waiting=[];
-    channel.members.forEach((k,v)=>{
-      let nick_id = [k.displayName,k.id,Math.floor(Math.random() * 1000)];
+  } else {
+    let waiting = [];
+    channel.members.forEach((k, v) => {
+      let nick_id = [k.displayName, k.id, Math.floor(Math.random() * 1000)];
       waiting.push(nick_id);
     });
-    waiting.sort((a,b)=>{
-      return a[2]>=b[2] ? 1: -1;
+    waiting.sort((a, b) => {
+      return a[2] >= b[2] ? 1 : -1;
     });
-    const half = channel.members.size/2;
-    let a_name=[];
-    let b_name=[];
-    let a_id=[];
-    let b_id=[];
-    for (let i=0; i<half; i++){
+    const half = channel.members.size / 2;
+    let a_name = [];
+    let b_name = [];
+    let a_id = [];
+    let b_id = [];
+    for (let i = 0; i < half; i++) {
       a_name.push(waiting[i][0]);
       a_id.push(waiting[i][1]);
-      b_name.push(waiting[i+half][0]);
-      b_id.push(waiting[i+half][1]);
+      b_name.push(waiting[i + half][0]);
+      b_id.push(waiting[i + half][1]);
     }
-    return [a_name,b_name,a_id,b_id];
+    return [a_name, b_name, a_id, b_id];
   }
-}
+};
--- db_api.js
+++ db_api.js
@@ -8,40 +8,39 @@ const notion = new dbClient.Client({
 });
 let top3List = [];
 
-var exports = module.exports = {};
+var exports = (module.exports = {});
 
 //Function
-exports.getAllUserData = async function() {
+exports.getAllUserData = async function () {
   const res = await notion.databases.query({
     database_id: dbID,
   });
   return res.results;
-}
+};
 
-exports.getTop3 = async function(dir){
+exports.getTop3 = async function (dir) {
   top3List = [];
   const res = await notion.databases.query({
-      database_id: dbID,
-      sorts: [
+    database_id: dbID,
+    sorts: [
       {
-          property: "power",
-          direction: dir ? "descending" : "ascending",
+        property: "power",
+        direction: dir ? "descending" : "ascending",
       },
-      ],
+    ],
   });
   let cnt = 0;
   res.results.forEach((item) => {
     if (cnt === 3) {
-    return;
+      return;
     }
     top3List.push(item.properties);
     cnt += 1;
-  }
-  );
+  });
   return top3List;
-}
+};
 
-exports.addToDatabase = async function(username){
+exports.addToDatabase = async function (username) {
   let check = false;
   const res = await notion.search({
     query: username,
@@ -79,6 +78,10 @@ exports.addToDatabase = async function(username){
             type: "number",
             number: 0,
           },
+          streak: {
+            type: "number",
+            number: 0,
+          },
         },
       });
     } catch (error) {
@@ -88,46 +91,46 @@ exports.addToDatabase = async function(username){
   } else {
     return false;
   }
-}
+};
 
-exports.searchUser = async function(userName) {
+exports.searchUser = async function (userName) {
   const res = await notion.search({
     query: userName,
   });
   return res;
-}
+};
 
-exports.updateValue = async function(originalData, state) {
+exports.updateValue = async function (originalData, state) {
   switch (state) {
     case "win":
-    await notion.pages.update({
+      await notion.pages.update({
         page_id: originalData.results[0].id,
         properties: {
-        win: {
+          win: {
             type: "number",
             number: (originalData.results[0].properties.win.number += 1),
-        },
-        power: {
+          },
+          power: {
             type: "number",
             number: (originalData.results[0].properties.power.number += 1),
+          },
         },
-        },
-    });
-    break;
+      });
+      break;
     case "lose":
-    await notion.pages.update({
+      await notion.pages.update({
         page_id: originalData.results[0].id,
         properties: {
-        lose: {
+          lose: {
             type: "number",
             number: (originalData.results[0].properties.lose.number += 1),
-        },
-        power: {
+          },
+          power: {
             type: "number",
             number: (originalData.results[0].properties.power.number -= 1),
+          },
         },
-        },
-    });
-    break;
+      });
+      break;
   }
-}
+};
